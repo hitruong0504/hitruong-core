d638d7f2bc4704625906b78adfb2acdc4adab18a
hitruong0504 <72957243+hitruong0504@users.noreply.github.com>
2021-03-09 19:11:03 +0700

    Update bai6.java

--- h520h0356lab2/bai6.java
+++ h520h0356lab2/bai6.java
@@ -6,18 +6,15 @@ public class bai6 {
         int c = 0;
         for (int i = 0; i < arr.length; i++) {
             if (k == arr[i])
-                c++;
-        }
-        if (c == 0) {
-            return -1;
-        } else {
-            return c;
+                return i + 1;
+            else
+                return -1;
         }
     }
     public static void main(String[] args) {
         int arr[] = { 0, 1, 3, 5, 7, 9, 11, 12, 14, 16 };
         Scanner sc = new Scanner(System.in);
         int k = sc.nextInt();
-        System.out.println(count(k, arr));
+        System.out.println(k + " thuoc vi tri thu " + count(k, arr) + " trong mang");
     }
 }
