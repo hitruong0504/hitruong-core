174d0e40a65435f4843c1898d7dcd88741fb446e
HiTruong <hitruong890@gmail.com>
2021-03-18 19:41:38 +0700

    Java

--- Java/OOP/Lab2/bai6.java
+++ Java/OOP/Lab2/bai6.java
@@ -1,15 +1,23 @@
+import java.util.Scanner;
+
+public class bai6 {
+    static int count(int k, int arr[])
+    {
         int c = 0;
         for (int i = 0; i < arr.length; i++) {
             if (k == arr[i])
-                return i + 1;
-            else
-                return -1;
+                c++;
+        }
+        if (c == 0) {
+            return -1;
+        } else {
+            return c;
         }
     }
     public static void main(String[] args) {
         int arr[] = { 0, 1, 3, 5, 7, 9, 11, 12, 14, 16 };
         Scanner sc = new Scanner(System.in);
         int k = sc.nextInt();
-        System.out.println(k + " thuoc vi tri thu " + count(k, arr) + " trong mang");
+        System.out.println(count(k, arr));
     }
 }
