63705f0bf2881dc4560c304e9e91a1bee149fc09
hitruong0504 <hitruong890@gmail.com>
2021-09-18 10:16:17 +0700

    Lab1

--- Data Structures and Algorithms/Lab1/MyLinkedList.java
+++ Data Structures and Algorithms/Lab1/MyLinkedList.java
@@ -17,6 +17,7 @@ public class MyLinkedList<E> implements ListInterface<E> {
     */
 
     @Override
+    // adđ
 
     public void addFirst(E item) {
         head = new Node<E>(item, head);
@@ -35,7 +36,7 @@ public class MyLinkedList<E> implements ListInterface<E> {
 
     public void addLast(E item) {
         if (head == null) { // is list empty?
-            addFirst(item); 
+            addFirst(item);
         } else {
             Node<E> tmp = head;
 
@@ -49,5 +50,99 @@ public class MyLinkedList<E> implements ListInterface<E> {
             numNode++;
         }
     }
+    
+    // remove
+    
+    public E removeFirst() throws NoSuchElementException {
+        if (head == null) {
+            throw new NoSuchElementException("Can't remove element from an empty list");
+            //return null;
+        } else {
+            Node<E> tmp = head;
+            head = head.getNext();
+            numNode--;
+            return head.getData();
+        }
+    }
 
+    public E removeAfter(Node<E> cur) throws NoSuchElementException {
+        if (cur == null) {
+            throw new NoSuchElementException("Can't remove element from an empty list");
+        } else {
+            Node<E> delNode = cur.getNext();
+            if (delNode != null) {
+                cur.setNext(delNode.getNext());
+                numNode--;
+                return delNode.getData();
+            } else {
+                throw new NoSuchElementException("No next node to remove");
+            }
+        }
+    }
+    
+    public E removeLast() throws NoSuchElementException {
+        if (head == null) {
+            throw new NoSuchElementException("Can't remove element from an empty list");
+        } else {
+            Node<E> prevNode = null;
+            Node<E> delNode = head;
+            while (delNode.getNext() != null) {
+                prevNode = delNode;
+                delNode = delNode.getNext();
+            }
+            prevNode.setNext(delNode.getNext());
+            delNode.setNext(null);
+            numNode--;
+            return delNode.getData();
+        }
+    }
+
+    //print
+    public void print() {
+        if (head == null) {
+            System.out.println("List is empty!!!");
+        } else {
+            Node<E> tmp = head;
+            System.out.print("List: " + tmp.getData());
+            tmp = tmp.getNext();
+            while (tmp != null) {
+                System.out.print(" -> " + tmp.getData());
+                tmp = tmp.getNext();
+            }
+            System.out.println();
+        }
+    }
+
+    public boolean isEmpty() {
+        numNode = 0 ? true : false;
+    }
+
+    public E getFirst() throws NoSuchElementException {
+
+        if (head == null) {
+            throw new NoSuchElementException("Can't remove element from an empty list");
+        } else {
+            return head.getData();
+        }
+    }
+    
+    public Node<E> getHead() {
+        return this.head;
+    }
+
+    
+    public int size() {
+        return this.numNode;
+    }
+
+    public boolean contains(E item) {
+        Node<E> tmp = head;
+        while (tmp != null) {
+            if (tmp.getData().equals(item)) {
+                return true;
+            }
+            tmp = tmp.getNext();
+        }
+        return false;
+    }
 }
