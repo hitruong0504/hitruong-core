785579ec69227aedd727cf43c998da07553b97cc
Hi Truong <hitruong890@gmail.com>
2022-03-16 21:34:34 +0700

    Updated

--- SQL/Lab3.sql
+++ SQL/Lab3.sql
@@ -13,11 +13,14 @@ select s.sid, s.name, c.cid, c.cname, r.score from student s, courses c, results
 where s.sid = r.sid and c.cid = r.cid and s.did = 'IT'
 -- 6. Display all student IDs in the student table; course id, student's course score.
 --    If the student has no score, the course id and score columns are null (Hint: using … join).
-select s.sid, r.cid, r.score from student s, results r
-where s.sid = r.sid
+select s.sid, c.cid, r.score 
+from student s cross join
+	 courses c left join
+	 results r
+	 on r.sid = s.sid and r.cid = c.cid
+select * from courses
 -- 7. List the courses that do not have previous courses.
-select c.cid from courses c, condition cond
-where c.cid = cond.precid
+select cid from courses c where c.cid not in (select distinct cid from condition) 
 -- 8. Show the number of students in each Department.
 select d.did, count(s.sid) from department d left join student s on d.did = s.did
 group by d.did order by count(s.sid) desc
@@ -35,5 +38,7 @@ select r.sid, s.name, avg(r.score) as 'avg' from results r, student s where s.si
 group by r.sid, s.name
 -- 13. Show the highest average.
 select max(avg_score) from (select avg(score) as avg_score from results group by sid) as maxScore
+select top 1 avg(score) from results group by sid order by avg(score) desc
 -- 14. Show the lowest average of the students.
 select min(avg_score) from (select avg(score) as avg_score from results group by sid) as minScore
+select top 1 avg(score) from results group by sid order by avg(score) asc
